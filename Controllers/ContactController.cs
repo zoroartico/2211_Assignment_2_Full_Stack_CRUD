8aa2fdfca372418955749425cb82af50d6a98f93
kaden-d <kaden-d@hotmail.com>
2024-02-12 00:00:10 -0700

    Retrofit class example of movie list to contact list. now have functioning baseline to work on assignment outlines.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _2211_Assignment_2_Full_Stack_CRUD.Models;
+
+namespace ContactList.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactContext context { get; set; }
+        public ContactController(ContactContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Categories =
+                context
+                .Categories
+                .OrderBy(c => c.CategoryName)
+                .ToList();
+            return View("Edit", new Contact());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Categories =
+                context
+                .Categories
+                .OrderBy(c => c.CategoryName)
+                .ToList();
+            var contact = context.Contacts.Find(id);
+            return View(contact);
+        }
+        [HttpPost]
+        public IActionResult Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                if (contact.ContactId == 0)
+                    context.Contacts.Add(contact);
+                else
+                    context.Contacts.Update(contact);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action =
+                (contact.ContactId == 0) ? "Add" : "Edit";
+                ViewBag.Categories =
+                    context
+                    .Categories
+                    .OrderBy(c => c.CategoryName)
+                    .ToList();
+                return View(contact);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var contact = context.Contacts.Find(id);
+            return View(contact);
+        }
+        [HttpPost]
+        public IActionResult Delete(Contact contact)
+        {
+            context.Contacts.Remove(contact);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,24 @@
-using _2211_Assignment_2_Full_Stack_CRUD.Models;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
+using Microsoft.EntityFrameworkCore;
+using _2211_Assignment_2_Full_Stack_CRUD.Models;
 namespace _2211_Assignment_2_Full_Stack_CRUD.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private ContactContext context { get; set; }
+        public HomeController(ContactContext ctx)
         {
-            _logger = logger;
+            context = ctx;
         }
-
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var contacts =
+                context
+                .Contacts.Include(c => c.Category)
+                .OrderBy(c => c.FirstName)
+                .ToList();
+            return View(contacts);
         }
     }
 }
