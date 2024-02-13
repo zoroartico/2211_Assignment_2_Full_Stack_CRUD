efa94aff8c73f025e606447fd3d26886d63224c3
kaden-d <kaden-d@hotmail.com>
2024-02-12 17:05:16 -0700

    added link styling in index. added regex email validation in contact model

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace _2211_Assignment_2_Full_Stack_CRUD.Models
 {
@@ -7,7 +9,11 @@ namespace _2211_Assignment_2_Full_Stack_CRUD.Models
         public int ContactId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        //uses regex to check for characters before and after an @, then checks for period and 2 or more chars.
+        [RegularExpression(@"^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$",ErrorMessage = "The Email field is invalid.")]
         public string Email { get; set; }
+
         public string Phone { get; set; }
         public string? Organization { get; set; }
         public int CategoryId {  get; set; }
